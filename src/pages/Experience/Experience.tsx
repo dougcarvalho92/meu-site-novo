c64a6d17105b3d722bf0ed3e2d0825a7e81056b3
dougcarvalho92 <dscarvalho199@gmail.com>
2020-10-19 18:01:16 -0300

    removendo imports não utilizados

--- src/pages/Experience/Experience.tsx
+++ src/pages/Experience/Experience.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+
 import Sidebar from "../../components/Sidebar";
 import api from "../../services/api";
 
